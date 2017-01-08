3204b1b03fd65b6f83ab30cd8823e822454e078d
guetta14 <guettaguy@gmail.com>
2017-01-08 15:14:39 +0200

    Update GherkinTest.java

--- src/test/java/GherkinTest.java
+++ src/test/java/GherkinTest.java
@@ -4,9 +4,9 @@ import org.junit.runner.RunWith;
 /**
  * Created by Guy Guetta on 08/01/2017.
  */
-// @RunWith(OctaneCucumber.class)
-// @CucumberOptions(plugin={"junit:junitResult.xml"},
-//        features="src/test/java/features")
+@RunWith(OctaneCucumber.class)
+@CucumberOptions(plugin={"junit:junitResult.xml"},
+       features="src/test/java/features")
 public class GherkinTest {
 
 }
