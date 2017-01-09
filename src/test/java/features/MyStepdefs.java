dcea5d04b17a58197faf1505d87d30212c15f67b
Guy Guetta <guy.guetta@hpe.com>
2017-01-09 17:29:23 +0200

    number of files to commit changes on = 4

--- src/test/java/features/MyStepdefs.java
+++ src/test/java/features/MyStepdefs.java
@@ -1,14 +1,19 @@
 package features;
 
 import cucumber.api.PendingException;
-import cucumber.api.java.en.When;
 
 /**
  * Created by Guy Guetta on 08/01/2017.
  */
 public class MyStepdefs {
-    @When("^there is a backlog with features and user stories$")
+//    @When("^there is a backlog with features and user stories$")
     public void thereIsABacklogWithFeaturesAndUserStories() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+//    @And("^there is an application module tree$")
+    public void thereIsAnApplicationModuleTree() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
