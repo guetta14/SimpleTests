99bff241297325e8342ad5ef2f692fda6349f789
Guy Guetta <guy.guetta@hpe.com>
2017-02-08 11:50:29 +0200

    number of files to commit changes on = 2

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,8 +1,5 @@
 import junit.framework.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.Random;
 
 
 /**
@@ -23,171 +20,171 @@ public class SimpleTest {
         Assert.assertEquals(1, 1);
     }
 
-    @Test
-    public void alwaysFailGuy1()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy2()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy3()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy4()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy5()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy6()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy7()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Test
-    public void alwaysFailGuy8()
-    {
-        Assert.assertEquals(1, 0);
-    }
-
-    @Ignore
-    @Test
-    public void ignoredGuy1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Ignore
-    @Test
-    public void sometimesPassGuy2() throws InterruptedException
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy3()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy5()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy6()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy7()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy8()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy9()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy10()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy11()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy12()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy13()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
-
-    @Test
-    public void sometimesPassGuy14()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFifee, true);
-    }
+//    @Test
+//    public void alwaysFailGuy1()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy2()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy3()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy4()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy5()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy6()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy7()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Test
+//    public void alwaysFailGuy8()
+//    {
+//        Assert.assertEquals(1, 0);
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void ignoredGuy1()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void sometimesPassGuy2() throws InterruptedException
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy3()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy5()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy6()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy7()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy8()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy9()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy10()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy11()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy12()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy13()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
+//
+//    @Test
+//    public void sometimesPassGuy14()
+//    {
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(100);
+//        boolean smallerThanFifee =  randomNumber < 50 ? true : false;
+//        Assert.assertEquals(smallerThanFifee, true);
+//    }
 
 }
